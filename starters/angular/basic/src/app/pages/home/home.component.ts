844adbe10823cd2624b6fbbf021484a8060a85a0
maksymtak <maksym.nielubowicz@gmail.com>
2026-05-11 16:05:50 +0200

    Trying to understand ts files

--- starters/angular/basic/src/app/pages/home/home.component.ts
+++ starters/angular/basic/src/app/pages/home/home.component.ts
@@ -9,16 +9,32 @@ import { YouTubeIconComponent } from '../../components/icons/youtube-icon.compon
 const icons = [ArrowBoxIconComponent, GithubIconComponent, TwitterIconComponent, YouTubeIconComponent];
 
 @Component({
-    selector: 'app-home',
+    selector: 'app-home',   
     imports: [RouterLink, ExternalLinkDirective, ...icons],
-    templateUrl: './home.component.html',
+    //templateUrl: './home.component.html',
+    template: '
+    
+    <section class="newShit">
+        <button class="sign-in" (click)="testingButton()"> Sign in </button>
+        <button class="sign-in" ><a href="google.com" ></a>Other button</button>
+        <a href="google.com"> go out </a>
+
+    </section>
+
+    <h1>Count: {{testingCounter}} </h1>
+        
+    
+    '
+    
     styleUrl: './home.component.scss',
     host: {
         'class': 'content'
     }
 })
 export class HomeComponent {
+    testingCounter = 0
+
     testingButton(): void{
-        
+        this.testingCounter += 1
     }
 }
